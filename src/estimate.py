09e1024eeff748a8572e3fc3d71b83ccbb7b5ce9
William Arliss <williamarliss@MacBook-Air.local>
2026-01-29 13:20:30 -0500

    sbm_fast_drop to automatically identify the optimal number of communities

--- src/estimate.py
+++ src/estimate.py
@@ -231,3 +231,136 @@ def sbm_fast(G, k, *,
     if track_scores:
         return partition, np.array(trace)
     return partition
+
+
+# This approach extends sbm_fast by iteratively dropping small communities
+# until the appropriate number of communities is found
+def sbm_fast_drop(G, *,
+                  min_size=3,
+                  likelihood='bernoulli',
+                  alpha=0.,
+                  gamma=1.,
+                  weight=None,
+                  track_scores=False,
+                  max_iter=100,
+                  min_iter=10,
+                  tol=0.01):
+
+    ## Adjacency matrix ##
+    G = G.to_directed()
+    A = nx.to_scipy_sparse_array(G, weight=weight).astype(float)
+    n_nodes = len(G.nodes)
+    n_comms = n_nodes // min_size
+
+    if likelihood == 'bernoulli':
+        assert ((A.data==0) | (A.data==1)).all()
+    elif likelihood == 'poisson':
+        assert (A.data >= 0).all() and (A.data == A.data.round()).all()
+    elif likelihood == 'normal':
+        pass
+    else:
+        raise ValueError
+
+    ## Soft partition matrix ##
+    X = np.ones((n_nodes, n_comms)) / n_comms
+    X += np.random.randn(n_nodes, n_comms) / 100
+    ## Hard partition matrix ##
+    partition = X.argmax(1)
+    Z = csr_array((np.ones(n_nodes), (np.arange(n_nodes), partition)),
+                  shape=X.shape, dtype=X.dtype)
+
+    ## Structure matrix sufficient statistics ##
+    M = (Z.T @ (A @ Z)).toarray()
+    n = Z.sum(0)[:, None]
+    ## Structure matrix MLE ##
+    B = M / (n@n.T).clip(1, None)
+
+    ## Regularization ##
+    R = np.eye(n_comms) * alpha
+
+    A2 = None # elementwise square of A
+    if track_scores:
+        vprint('tracking scores may increase runtime')
+        ## Initialize trace of scores ##
+        if likelihood == 'bernoulli':
+            L = M * clog(B) + (n@n.T - M) * clog(1-B)
+        elif likelihood == 'poisson':
+            L = M * clog(B) - (n@n.T) * B
+        elif likelihood == 'normal':
+            A2 = A.multiply(A)
+            M2 = (Z.T @ (A2 @ Z)).toarray()
+            L = -1/2 * (M2 - 2*B*M + (n@n.T) * B**2)
+        trace = [L.sum()/n_nodes**2]
+
+    for epoch in range(max_iter):
+
+        ## Compute predictions ##
+        if likelihood == 'bernoulli':
+            w_pre = 1 / (B * (1 - B)).clip(EPS, None)
+        elif likelihood == 'poisson':
+            w_pre = 1 / B.clip(EPS, None)
+        elif likelihood == 'normal':
+            w_pre = np.ones_like(B)
+        w_block = (w_pre * n.T).sum(axis=1) / n.sum()
+        w = w_block[partition]
+
+        ## Compute gradients and hessian ##
+        # ZB = B.T[partition, :]
+        ZB = Z @ B.T
+        ZBW = ZB * w[:, None]
+        hess = ZB.T @ ZBW + R
+        # grad = ZBW.T @ A
+        grad = (A.T @ ZBW).T
+
+        ## Modify derivatives with entropy penalty ##
+        p = Z.mean(0).clip(EPS, None)
+        grad -= gamma * (np.log(p) + 1)[:, None]
+        hess -= gamma * np.diag(1 / p)
+
+        ## Perform fisher scoring updates ##
+        X = (X - Z) + np.linalg.solve(hess, grad).T
+
+        ## Update partition ##
+        prev_partition = partition
+        partition = X.argmax(1)
+
+        ## Get rid of unused communities for stability ##
+        mask = np.bincount(partition, minlength=n_comms) >= min_size
+        if (~mask).any():
+            X = X[:, mask]
+            partition = X.argmax(1)
+            n_comms = X.shape[1]
+            R = np.eye(n_comms) * alpha
+            Z = csr_array((np.ones(n_nodes), (np.arange(n_nodes), partition)),
+                          shape=X.shape, dtype=X.dtype)
+        else:
+            Z.indices[:] = partition
+            Z.data[:] = 1
+
+        ## Recompute structure matrix ##
+        M = (Z.T @ (A @ Z)).toarray()
+        n = Z.sum(0)[:, None]
+        B = M / (n@n.T).clip(1, None)
+
+        ## Early stopping ##
+        if epoch >= min_iter and (prev_partition == partition).mean() > 1-tol:
+            vprint('converged in', epoch+1, 'iterations')
+            break
+
+        ## Append current score to trace ##
+        if track_scores:
+            if likelihood == 'bernoulli':
+                L = M * clog(B) + (n@n.T - M) * clog(1-B)
+            elif likelihood == 'poisson':
+                L = M * clog(B) - (n@n.T) * B
+            elif likelihood == 'normal':
+                M2 = (Z.T @ (A2 @ Z)).toarray()
+                L = -1/2 * (M2 - 2*B*M + (n@n.T) * B**2)
+            trace.append(L.sum()/n_nodes**2)
+
+    else:
+        vprint('did not converge after', max_iter, 'iterations')
+
+    if track_scores:
+        return partition, np.array(trace)
+    return partition
